e9015e4005cddef68d826c301cbb1317dac561eb
Hussain Rao <hussainrao7599@gmail.com>
2025-12-18 17:03:52 +0530

    Update st 1.py

--- st 1.py
+++ st 1.py
@@ -6,10 +6,8 @@ st.set_page_config(page_title="Sentiment Analysis App", page_icon="😊")
 st.title("Sentiment Analysis App")
 st.write("Enter any text and check its sentiment")
 
-# Text input
 user_text = st.text_area("Enter your text here")
 
-# Analyze button
 if st.button("Analyze Sentiment"):
     if user_text.strip() == "":
         st.warning("Please enter some text")
@@ -23,15 +21,14 @@ if st.button("Analyze Sentiment"):
         if polarity > 0:
             st.success("Positive Sentiment 😊")
             st.image("pos_senti.png")
+
         elif polarity < 0:
             st.error("Negative Sentiment 😠")
             st.image("neg_senti.png")
+
         else:
             st.info("Neutral Sentiment 😐")
             st.image("neut_senti.png")
+
 st.markdown("---")
 st.caption("Built with Streamlit & TextBlob")
-
-
-
-
